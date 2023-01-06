00d7af0ea171a827a2f7f245e330467064e35697
Prins-Verma <princeverma.gzp786@gmail.com>
2023-01-06 20:37:49 +0530

    TextEditor updated

--- TextEditor/src/App.js
+++ TextEditor/src/App.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 //import { Router, Route } from 'react-router'
 // import ReactDOM from 'react-dom';
 // import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
-import {  
-  BrowserRouter as Router,    
-  Routes,  
-  Route
-}   
-from 'react-router-dom';  
+// import {  
+//   BrowserRouter as Router,    
+//   Routes,  
+//   Route
+// }   
+// from 'react-router-dom';  
 
 function App() {
   const [mode, setMode] = useState('light'); //mode checking 
@@ -48,24 +48,26 @@ function App() {
   return (
     <>
       {/* Giving a name of Navbar which is TextUtils through props */}
-  <Router>
+  {/* <Router> */}
   
         <Navbar title="TextEditor" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my - 3"  >
 
       {/* "exact path" is used to match exact path which we want to render instead "path" ex: /users  and /users/home both are different */}
-            <Routes>                    
-                <Route path= "/" element= {<TextForm showAlert={showAlert} heading="Enter your text below to analyze" mode={mode} />}>
+            {/* <Routes>                    
+                <Route path= "/" element= 
                 </Route>
 
                 <Route path= "/about" element= {<About />}>
                 </Route>
-            </Routes>
+            </Routes> */}
+            <TextForm showAlert={showAlert} heading="Enter your text below to analyze" mode={mode} />
+
 
        </div>
 
-  </Router>
+  {/* </Router> */}
 
     </>
   );
--- TextEditor/src/components/Navbar.js
+++ TextEditor/src/components/Navbar.js
@@ -1,7 +1,7 @@
 // This file purely contains navbar code 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 
 // Using props as parameter in function
@@ -12,18 +12,18 @@ export default function Navbar(props) {
   <div className="container-fluid">
 
   {/* using props title from Apps.js */}
-    <Link className="navbar-brand" to="/">{props.title}</Link>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/about">About</Link>
-        </li>
+        {/* <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/about">About</a>
+        </li> */}
       </ul>
       
       <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark': 'light'}`}>
